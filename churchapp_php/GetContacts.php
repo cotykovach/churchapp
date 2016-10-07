97eab5df7f7467fbef6cc66df7d2be1c61d34c7c
cotykovach <cotykovach@gmail.com>
2016-10-07 07:56:00 -0500

    Add files via upload

---
+++ churchapp_php/GetContacts.php
@@ -0,0 +1,49 @@
+<?php
+	
+$servername = "localhost";
+$username = "cotykova_COTY";
+$password = "Scrapmonkey722!";
+$dbname = "cotykova_church_test";
+	
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+	
+	else
+	{
+		//Create query to retrieve all contacts
+		$query = 'SELECT * FROM Contacts ORDER BY Contact_Priority';
+		
+		$stmt = mysqli_query($conn, $query);
+		
+		if (!$stmt)
+		{
+			//Query failed
+			echo 'Query failed';
+		}
+		
+		else
+		{
+			$contacts = array(); //Create an array to hold all of the contacts
+			//Query successful, begin putting each contact into an array of contacts
+			
+			while ($row = mysqli_fetch_array($stmt,MYSQLI_ASSOC)) //While there are still contacts
+			{
+				//Create an associative array to hold the current contact
+				//the names must match exactly the property names in the contact class in our C# code.
+				$currentContact = array("ID" => $row['Contact_ID'],
+								 "Name" => $row['Contact_Name'],
+								 "Image" => $row['Contact_ImagePath']
+								 );
+								 
+				//Add the contact to the contacts array
+				array_push($contacts, $currentContact);
+			}
+			
+			//Echo out the contacts array in JSON format
+			echo json_encode($contacts);
+		}
+	}
+?>
---
+++ churchapp_php/GetSermonDetail.php
@@ -0,0 +1,61 @@
+<?php
+
+
+
+	//Get the POST variables
+	$mSermonID = $_POST['SermonID'];
+	
+$servername = "localhost";
+$username = "cotykova_COTY";
+$password = "Scrapmonkey722!";
+$dbname = "cotykova_church_test";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+	
+	else
+	{
+		//Create query to retrieve all contacts
+		
+		
+		$query = "SELECT * FROM Sermon WHERE Sermon_ID=('".$mSermonID."')";
+		
+		$stmt = mysqli_query($conn, $query);
+		
+		if (!$stmt)
+		{
+			//Query failed
+			echo 'Query failed';
+		}
+		
+		else
+		{
+			 //Create an array to hold all of the contacts
+			//Query successful, begin putting each contact into an array of contacts
+			
+			$row = mysqli_fetch_array($stmt,MYSQLI_ASSOC); //While there are still contacts
+			
+				//Create an associative array to hold the current contact
+				//the names must match exactly the property names in the contact class in our C# code.
+				$currentsermon = array("Title" => $row['Sermon_Title'],
+								 "Date" => $row['Sermon_Date'],
+								 "Image"=> $row['Sermon_ImagePath'],
+								 "Text"=> $row['Sermon_TextPath']
+								 );
+								 
+				//Add the contact to the contacts array
+				
+			
+			
+			//Echo out the contacts array in JSON format
+			echo json_encode($currentsermon);
+			mysqli_close($conn);
+		}
+	}
+
+?>
